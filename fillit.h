3b7351d891c958910b60395fb978df796cd855a9
Florian Finet <flfinet@z3r2p4.le-101.fr>
2018-10-30 15:05:24 +0100

    test

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: flfinet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/08 18:29:59 by flfinet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/29 12:22:52 by flfinet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/30 15:05:15 by flfinet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,5 +19,14 @@
 # include <stdlib.h>
 
 int main(int ac, char **av);
+
+typedef struct s_tetri
+{
+  int width;
+  int height;
+  char **content;
+  struct s_tetri *next;
+  struct s_tetri *prev;
+} t_tetri;
 
 #endif
